7ef598e71d7049433a54ba908cf4ea6fe66bfe83
Dan Morgan <dan@dmorgan.xyz>
2026-03-11 16:16:08 +0000

    Initial Commit

---
+++ WageTickTests/WageTickTests.swift
@@ -0,0 +1,16 @@
+//
+//  WageTickTests.swift
+//  WageTickTests
+//
+//  Created by Dan Morgan on 11/03/2026.
+//
+
+import Testing
+
+struct WageTickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
